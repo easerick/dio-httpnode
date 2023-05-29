1ba3fb1e64fee4b77f63ae64295ccb4837443cdc
easerick <ericksvip@gmail.com>
2023-05-29 18:11:54 -0300

    Projeto Iaac com Serverless Framework na AWS

---
+++ src/fetchitems.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const AWS = require("aws-sdk");
+
+const fetchItems = async (event) => {
+  
+    const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+        let items;
+
+    try {
+        const result = await dynamoDB.scan({
+            TableName: "ItemTableNew",
+        }).promise();
+
+        items = result.Item;
+    } catch (error) {
+
+        console.log(error)
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(items),
+    };
+};
+
+module.exports = {
+    handler: fetchItems,
+};
